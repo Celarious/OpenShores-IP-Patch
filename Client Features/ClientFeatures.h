7c3e0f643dfc5e416c8f94ecfe6cef0244ca8665
Celarious <53405029+Celarious@users.noreply.github.com>
2026-08-17 05:09:33 +0100

    Add ClientFeatures.h for ConfigureScrollingText() declaration

---
+++ Client Features/ClientFeatures.h
@@ -0,0 +1,17 @@
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the CLIENTINTERFACE_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// CLIENTINTERFACE_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#pragma once
+
+#ifdef CLIENTFEATURES_EXPORTS
+#define CLIENTFEATURES_API __declspec(dllexport)
+#else
+#define CLIENTFEATURES_API __declspec(dllimport)
+#endif
+
+#include <QString>
+
+CLIENTFEATURES_API void ConfigureScrollingText(void* scrollingText);
