f01e01becfcc9a35cef52e671607cf3a2fd3b026
Celarious <53405029+Celarious@users.noreply.github.com>
2026-08-17 05:13:38 +0100

    Enhance ConfigureScrollingText with comments

--- Client Features/ClientFeatures.cpp
+++ Client Features/ClientFeatures.cpp
@@ -10,12 +10,12 @@ void ConfigureScrollingText(void* scrollingText)
 
     QList<QString>* textList =
         reinterpret_cast<QList<QString>*>(
-            static_cast<char*>(scrollingText) + 0x30
+            static_cast<char*>(scrollingText) + 0x30 // RSI register passed through RCX (win x64 convention)
             );
 
-    textList->clear();
+    textList->clear(); // Clears the original scrolling text list
 
-    textList->append(QString::fromLatin1("*OpenShores"));
+    textList->append(QString::fromLatin1("*OpenShores")); // The * at the start of the string is a marker that the game checks for, and if present, removes it and centers + boldens the line
     textList->append(QString::fromLatin1("Welcome to OpenShores"));
     textList->append(QString::fromLatin1("V0.0.5 (2026)"));
 }
